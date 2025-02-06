788e8d3db5985b348b95a7c590a868b82a04b010
Harsh Kumar Sinha <hksinha960@gmail.com>
2025-02-06 17:14:15 +0530

    Login

--- backend/parser/model/user.js
+++ backend/parser/model/user.js
@@ -1,1 +1,26 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
+const adminsSchema = new Schema(
+  {
+    uid: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    photoURL: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const webadmin = mongoose.model("admin", adminsSchema);
+export default webadmin;
